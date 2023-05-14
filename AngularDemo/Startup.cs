093b3d3c2f3d3145071d6ad67a8c86d550abd3ab
Ivan Gamanovich <igamanovich@elinext.com>
2023-05-14 15:49:43 +0300

    feat: add basic files

---
+++ AngularDemo/Startup.cs
@@ -0,0 +1,16 @@
+﻿using AngularDemo.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace AngularDemo
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        => services.AddDbContext<AppDBContext>();
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+        }
+    }
+}
